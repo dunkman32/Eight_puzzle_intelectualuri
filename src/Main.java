df30c439207bc85b0b8b158ca0698b36938b2d4f
dunkman32 <dunkman032@gmail.com>
2018-04-17 17:24:34 +0400

    final

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     static final int[][] finalState = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 0}
+//            {1, 2, 3},
+//            {4, 5, 6},
+//            {7, 8, 0}
+            {2,8,3},
+            {1,6,4},
+            {7,0,5}
     };
 
     public static void main(String... args) {
@@ -23,6 +26,7 @@ public class Main {
             }
         }
         System.out.println(" შემოიტანე ცარიელი უჯრის კოორდინატები");
+
         System.out.print("  სვეტის ნომერი : ");
         zeroX = scanner.nextInt() - 1;
         System.out.print("  სტრიქონის ნომერი : ");
@@ -31,7 +35,8 @@ public class Main {
 
         BFS puzzle = new BFS(currentState, finalState);
 
-        puzzle.bfs(zeroX, zeroY);
+      //  puzzle.bfs(zeroX, zeroY);
+        puzzle.bfs(zeroY, zeroX);
     }
 }
 
